10747f950f740445473a2f2b69bda7fa6bb1f990
sn1p3r46 <kingokongo46@gmail.com>
2015-10-21 06:16:17 +0200

    test1

---
+++ src/assignment/Marshaller.java
@@ -0,0 +1,95 @@
+package assignment;
+
+/**
+ * Created by alexander on 20/10/15.
+ */
+
+import assignment.generated.*;
+
+import javax.xml.bind.*;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+
+import java.util.GregorianCalendar;
+public class Marshaller {
+    public void generateXMLDocument(File xmlDocument) throws java.text.ParseException, DatatypeConfigurationException {
+        try {
+
+            JAXBContext jaxbContext = JAXBContext.newInstance("assignment.generated");
+            javax.xml.bind.Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty("jaxb.formatted.output", true);
+            assignment.generated.ObjectFactory factory = new assignment.generated.ObjectFactory();
+
+            PeopleType people = factory.createPeopleType();
+
+
+            PersonType p = factory.createPersonType();
+            p.setId(new BigInteger("1"));
+            p.setFirstname("gianni");
+            p.setLastname("morandi");
+            p.setBirthdate(DatatypeFactory.newInstance().newXMLGregorianCalendar(new GregorianCalendar(1930, 2, 12)));
+            HealthProfileType healthProfile1 = factory.createHealthProfileType();
+            healthProfile1.setHeight(new Float("1.31"));
+            healthProfile1.setWeight(new Float("42"));
+            healthProfile1.setBmi(new Float("22.33"));
+            healthProfile1.setLastupdate(DatatypeFactory.newInstance().newXMLGregorianCalendar(2011, 3, 4, 0, 0, 0, 0, 0));
+            p.setHealthprofile(healthProfile1);
+
+
+            PersonType p2 = factory.createPersonType();
+            p2.setId(new BigInteger("2"));
+            p2.setFirstname("Anna");
+            p2.setLastname("Marini");
+            p2.setBirthdate(DatatypeFactory.newInstance().newXMLGregorianCalendar(new GregorianCalendar(1970, 6, 3)));
+            HealthProfileType healthProfile2 = factory.createHealthProfileType();
+            healthProfile2.setHeight(new Float("1.77"));
+            healthProfile2.setWeight(new Float("76"));
+            healthProfile2.setBmi(new Float("53.63"));
+            healthProfile2.setLastupdate(DatatypeFactory.newInstance().newXMLGregorianCalendar(2013, 3, 6, 0, 0, 0, 0, 0));
+            p2.setHealthprofile(healthProfile2);
+
+
+            PersonType p3 = factory.createPersonType();
+            p3.setId(new BigInteger("3"));
+            p3.setFirstname("Enza");
+            p3.setLastname("Set");
+            p3.setBirthdate(DatatypeFactory.newInstance().newXMLGregorianCalendar(new GregorianCalendar(2000, 6, 15)));
+            HealthProfileType healthProfile3 = factory.createHealthProfileType();
+            healthProfile3.setHeight(new Float("1.35"));
+            healthProfile3.setWeight(new Float("45"));
+            healthProfile3.setBmi(new Float("3.25"));
+            healthProfile3.setLastupdate(DatatypeFactory.newInstance().newXMLGregorianCalendar(2009, 11, 9, 0, 0, 0, 0, 0));
+            p3.setHealthprofile(healthProfile3);
+
+            people.getPerson().add(p);
+            people.getPerson().add(p2);
+            people.getPerson().add(p3);
+
+            JAXBElement<PeopleType> peopleElement = factory
+                    .createPeople(people);
+            System.out.println(peopleElement);
+            marshaller.marshal(peopleElement,
+                    new FileOutputStream(xmlDocument));
+
+        } catch (IOException e) {
+            System.out.println(e.toString());
+
+        } catch (JAXBException e) {
+            System.out.println(e.toString());
+
+        }
+
+    }
+
+
+
+    public static void main(String[] argv) throws DatatypeConfigurationException, java.text.ParseException {
+        String xmlDocument = "people1.xml";
+        Marshaller jaxbMarshaller = new Marshaller();
+        jaxbMarshaller.generateXMLDocument(new File(xmlDocument));
+    }
+}
---
+++ src/assignment/UnMarshaller.java
@@ -0,0 +1,87 @@
+package assignment;
+
+import assignment.generated.PeopleType;
+import assignment.generated.PersonType;
+import org.xml.sax.SAXException;
+import javax.xml.bind.*;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by alexander on 20/10/15.
+ */
+
+public class UnMarshaller {
+    public void unMarshall(File xmlDocument) {
+        try {
+
+            JAXBContext jaxbContext = JAXBContext.newInstance("assignment.generated");
+
+            Unmarshaller unMarshaller = jaxbContext.createUnmarshaller();
+            SchemaFactory schemaFactory = SchemaFactory
+                    .newInstance("http://www.w3.org/2001/XMLSchema");
+            Schema schema = schemaFactory.newSchema(new File("people.xsd"));
+            unMarshaller.setSchema(schema);
+            CustomValidationEventHandler validationEventHandler = new CustomValidationEventHandler();
+            unMarshaller.setEventHandler(validationEventHandler);
+
+            @SuppressWarnings("unchecked")
+            JAXBElement<PeopleType> peopleElement = (JAXBElement<PeopleType>) unMarshaller
+                    .unmarshal(xmlDocument);
+
+            PeopleType peopleType = peopleElement.getValue();
+
+
+            List<PersonType> peopleList = peopleType.getPerson();
+            for (int i = 0; i < peopleList.size(); i++) {
+
+                PersonType p = (PersonType) peopleList.get(i);
+
+                System.out.println(p.getId());
+                System.out.println(p.getLastname());
+                System.out.println(p.getFirstname());
+                System.out.println(p.getBirthdate().toString());
+                System.out.println(p.getHealthprofile().getHeight());
+                System.out.println(p.getHealthprofile().getWeight());
+                System.out.println(p.getHealthprofile().getBmi());
+                System.out.println(p.getHealthprofile().getLastupdate().toString());
+
+
+            }
+        } catch (JAXBException e) {
+            System.out.println(e.toString());
+        } catch (SAXException e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    public static void main(String[] argv) {
+        File xmlDocument = new File("people1.xml");
+        UnMarshaller jaxbUnmarshaller = new UnMarshaller();
+
+        jaxbUnmarshaller.unMarshall(xmlDocument);
+
+    }
+
+    class CustomValidationEventHandler implements ValidationEventHandler {
+        public boolean handleEvent(ValidationEvent event) {
+            if (event.getSeverity() == ValidationEvent.WARNING) {
+                return true;
+            }
+            if ((event.getSeverity() == ValidationEvent.ERROR)
+                    || (event.getSeverity() == ValidationEvent.FATAL_ERROR)) {
+
+                System.out.println("Validation Error:" + event.getMessage());
+
+                ValidationEventLocator locator = event.getLocator();
+                System.out.println("at line number:" + locator.getLineNumber());
+                System.out.println("Unmarshalling Terminated");
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
